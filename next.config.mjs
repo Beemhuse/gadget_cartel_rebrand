a1e0bf4dd20ba856ea2d069869f1980591c775a9
Beemhuse <Brightawah94@gmail.com>
2024-08-31 12:40:25 -0700

    feat: added pwa functionality

--- next.config.mjs
+++ next.config.mjs
@@ -22,10 +22,10 @@ const nextConfig = {
 // PWA Configuration
 const pwaConfig = {
   dest: 'public', // Destination directory for the PWA files
-//   disable: process.env.NODE_ENV === 'development', // Disable PWA in the development environment
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in the development environment
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
-  disable: false,
+  // disable: false,
 };
 
 // Export configuration with PWA
